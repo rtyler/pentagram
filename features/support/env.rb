b28679e7291c82a43b21f79bd7f8c615264efd15
R. Tyler Croy <tyler@monkeypox.org>
2012-06-17 13:42:37 -0700

    Further set up the temp directory to contain a modules symlink and bootstrap.sh

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
+require 'fileutils'
 require 'temp_dir'
-
 
 Before do
   # Before every scenario, we want to create a temporary directory that
@@ -8,6 +8,16 @@ Before do
   @original_dir = Dir.pwd
   @dir = TempDir.create(:basename => 'pentagram')
   Dir.chdir(@dir)
+
+  # Set up the modules symlink
+  FileUtils.ln_s(File.join(@original_dir, '/modules'),
+                 File.join(@dir, '/modules'))
+
+  # Copy the bootstrap.sh into the temporary directory so
+  # Blimpy can find and use it properly
+  FileUtils.cp(File.join(File.dirname(__FILE__), '/bootstrap.sh'),
+               @dir)
+  puts "Using temp dir: #{@dir}"
 end
 
 After do |scenario|
