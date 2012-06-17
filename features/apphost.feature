940541c01d84e9f042d0c2303bc9d175bac89c7e
R. Tyler Croy <tyler@monkeypox.org>
2012-06-17 13:17:31 -0700

    Create the first scenario: Provision a fresh app host
    Letting cucumber guide the way here, just taking the automatically generated
    steps and throwing them directly into a step file

--- features/apphost.feature
+++ features/apphost.feature
@@ -6,3 +6,11 @@ Feature: Serve the PentaGram web application
 
   App hosts should be configured to run the PentaGram
   web application
+
+
+  Scenario: Provision a fresh app host
+    Given an empty host
+    And the host is to become a PentaGram app host
+    When I provision the host
+    Then it should be running a web server
+    And it should be responding to web requests
---
+++ features/step_definitions/apphost_steps.rb
@@ -0,0 +1,20 @@
+Given /^an empty host$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the host is to become a PentaGram app host$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I provision the host$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should be running a web server$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should be responding to web requests$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
