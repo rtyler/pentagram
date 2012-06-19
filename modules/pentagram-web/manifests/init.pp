47386fdd8270294cbcdff609f07663db77532374
R. Tyler Croy <tyler@monkeypox.org>
2012-06-18 21:07:56 -0700

    First step of pentagram-web module, pull in apache
    This also requires some stupid symlinking set up for rspec-puppet
    to properly find other modules to load in

---
+++ modules/pentagram-web/manifests/init.pp
@@ -0,0 +1,5 @@
+class pentagram-web {
+  class {
+    'apache' : ;
+  }
+}
--- modules/pentagram-web/spec/classes/pentagram-web_spec.rb
+++ modules/pentagram-web/spec/classes/pentagram-web_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe 'pentagram-web' do
-
+  it 'must include the Puppet apache module' do
+    should include_class('apache')
+  end
 end
